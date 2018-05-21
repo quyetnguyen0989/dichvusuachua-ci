9d4c5f181703dcdd8f9c43e4166151ff5c7a3628
root <quyetnguyen0989@gmail.com>
2018-05-22 00:19:05 +0700

    Add full code

--- wp-config.php
+++ wp-config.php
@@ -36,6 +36,7 @@ define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+define('DISABLE_WP_CRON', 'true');
 
 /**#@+
  * Authentication Unique Keys and Salts.
