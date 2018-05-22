30dda0d7f6ff5b50c67e2db3f6b4978e350377f2
quyetnguyen0989 <quyetnguyen0989@gmail.com>
2018-05-22 11:24:49 +0700

    Up code final

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'suamaytinh_smt');
+define('DB_NAME', 'dichvusuachua');
 
 /** MySQL database username */
-define('DB_USER', 'suamaytinh_smt');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'P@ss123!');
+define('DB_PASSWORD', 'P@ss0987');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '172.18.0.3');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
